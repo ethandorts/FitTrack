344cbe03e4de12872a47e376ed633aca3ff3375b
Ethan Doherty <ethandorts2002@gmail.com>
2024-04-22 23:04:50 +0100

    Back4App integration and RegisterActivity layout added

---
+++ app/src/main/java/com/example/fittrack/App.java
@@ -0,0 +1,20 @@
+package com.example.fittrack;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
+        Log.d("message", "Server started");
+    }
+}
--- app/src/main/java/com/example/fittrack/LoginActivity.java
+++ app/src/main/java/com/example/fittrack/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.fittrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText Email = findViewById(R.id.edit_text_email);
+        EditText Password = findViewById(R.id.edit_text_password);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
